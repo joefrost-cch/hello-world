ca2f97ec71f77dcae1c3aa747bf56b6b0afd6ce2
joefrost-cch <joe@cornishcottageholidays.co.uk>
2017-04-27 15:34:20 +0100

    Update mainclasses.php

--- mainclasses.php
+++ mainclasses.php
@@ -1,10 +1,12 @@
 <?php
-namespace mainclass.php;
+namespace mainClass;
 class dbaseFunctions {
   function dbaseConnect() {
      
   }
 }
+$object = new \mainClass\dbaseFunctions;
 
+echo $object->dbaseConnect();
 
 ?>

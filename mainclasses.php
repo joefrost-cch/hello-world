62fc0be57fa41df72409f913dcee0912a40a41c5
joefrost-cch <joe@cornishcottageholidays.co.uk>
2017-04-27 15:46:38 +0100

    Update mainclasses.php

--- mainclasses.php
+++ mainclasses.php
@@ -7,4 +7,14 @@ class dBaseFunctions {
 }
 $object = new \mainClass\dbaseFunctions;
 echo json_encode($object->dbaseConnect());
+
+
+$checkMatch = "a12111Ha1";
+
+// preg_match uppercase [A-Z], numeral (\d)
+if (!preg_match("/^(?=.*[A-Z])(?=.*\d).+$/",$checkMatch)) {
+	echo "incorrect";
+} else {
+	
+}
 ?>
